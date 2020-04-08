7a1551d6e43b7bba7a464569d85631fd4e0c5210
Ignacio Ribas <iribas@itba.edu.ar>
2020-04-08 18:35:48 -0300

    VoteServiceImplementation

--- tpe_paw/services/src/main/java/ar/edu/itba/paw/services/VoteServiceImpl.java
+++ tpe_paw/services/src/main/java/ar/edu/itba/paw/services/VoteServiceImpl.java
@@ -1,7 +1,9 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.interfaces.dao.VoteDao;
 import ar.edu.itba.paw.interfaces.service.VoteService;
 import ar.edu.itba.paw.models.Vote;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -9,18 +11,22 @@ import java.util.Optional;
 
 @Service
 public class VoteServiceImpl implements VoteService {
+
+    @Autowired
+    VoteDao voteDao;
+
     @Override
     public Collection<Vote> getUserVotes(long userId) {
-        return null;
+        return voteDao.getUserVotes(userId);
     }
 
     @Override
     public Optional<Vote> getVote(long userId, long voteId) {
-        return Optional.empty();
+        return voteDao.getVote(userId, voteId);
     }
 
     @Override
     public void performVote(long userId, long voteId, int voteType) {
-
+        voteDao.performVote(userId, voteId, voteType);
     }
 }

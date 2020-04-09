2b7ce67290ae097a053ce5e3c2a452aa9f3ed7ad
Florencia Petrikovich <fpetrikovich@itba.edu.ar>
2020-04-08 21:50:28 -0300

    [CSS][Feed] Added the fade out on the cards when the code is too long. Other css changes as well

--- tpe_paw/webapp/src/main/webapp/resources/js/feedSnippets.js
+++ tpe_paw/webapp/src/main/webapp/resources/js/feedSnippets.js
@@ -1,6 +1,22 @@
 
 /* Resizes the height of a single card */
 function resizeCard(card){
+    /*
+     * CODE CONTAINER --> If the code is too long, will want it to fade out in the card
+     */
+    codeBlock = card.querySelector('.card-snippet-content').querySelector('.snippet-code-container').querySelector('.card-snippet-code-block');
+
+    codeBlockHeight = codeBlock.getBoundingClientRect().height;
+    codeBlockMaxHeight = 200;
+
+    if (codeBlockHeight >= (codeBlockMaxHeight)) {
+        /* Add the fade out */
+        codeBlock.querySelector('.card-snippet-fade-out').style.backgroundImage = 'linear-gradient(to bottom, transparent, #DCDCDC)';
+    }
+
+    /*
+     * CARD CONTAINER --> Making the card height match the content
+     */
     feedGrid = document.getElementsByClassName("feed-snippets-grid")[0];
 
     /* Get the height and gap of the cards */
@@ -11,12 +27,12 @@ function resizeCard(card){
     rowSpan = Math.ceil((card.querySelector('.card-snippet-content').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
 
     /* Add the new row span to the card style */
-    card.style.gridRowEnd = "span " + rowSpan;
+    card.style.gridRowEnd = 'span ' + rowSpan;
 }
 
 /* Get all the different cards and for each one, resize it to the correct height */
 function resizeAllCards(){
-    cardContainer = document.getElementsByClassName("card-snippet-container");
+    cardContainer = document.getElementsByClassName('card-snippet-container');
     for(i = 0; i < cardContainer.length; i++){
         resizeCard(cardContainer[i]);
     }
@@ -26,5 +42,4 @@ function resizeAllCards(){
 window.onload = resizeAllCards();
 
 /* Resize the cards again when the browser is resized */
-window.addEventListener("resize", resizeAllCards);
-
+window.addEventListener('resize', resizeAllCards);

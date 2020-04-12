3bae01eb2368554cda39f91e76661cfc5d73879e
Florencia Petrikovich <fpetrikovich@itba.edu.ar>
2020-04-12 16:44:00 -0300

    [Tags] Included the lists of tags in the snippet model

--- tpe_paw/models/src/main/java/ar/edu/itba/paw/models/Snippet.java
+++ tpe_paw/models/src/main/java/ar/edu/itba/paw/models/Snippet.java
@@ -1,6 +1,9 @@
 package ar.edu.itba.paw.models;
 
+import org.omg.IOP.TAG_CODE_SETS;
+
 import java.util.Calendar;
+import java.util.Collection;
 
 public class Snippet {
     private long id;
@@ -10,8 +13,9 @@ public class Snippet {
     private String description;
     private String dateCreated;
     private String language;
+    private Collection<Tag> tags;
 
-    public Snippet(long id, User owner, String code, String title, String description, String dateCreated, String language) {
+    public Snippet(long id, User owner, String code, String title, String description, String dateCreated, String language, Collection<Tag> tags) {
         this.id = id;
         this.owner = owner;
         this.code = code;
@@ -19,6 +23,7 @@ public class Snippet {
         this.description = description;
         this.dateCreated = dateCreated;
         this.language = language;
+        this.tags = tags;
     }
 
     public long getId() {
@@ -47,5 +52,9 @@ public class Snippet {
 
     public String getLanguage() {
         return language;
+    }
+
+    public Collection<Tag> getTags() {
+        return tags;
     }
 }

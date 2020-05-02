483041d0bde9af2783a521a8b9142b788407ba52
ahenestrosa <ahenestrosa@itba.edu.ar>
2020-05-01 23:17:59 -0300

    [Testing] Corrections to tests, separation of evaluation and precond

--- tpe_paw/persistence/src/test/java/ar/edu/itba/paw/persistence/SnippetDaoTest.java
+++ tpe_paw/persistence/src/test/java/ar/edu/itba/paw/persistence/SnippetDaoTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -21,6 +22,8 @@ import javax.sql.DataSource;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import static junit.framework.TestCase.*;
@@ -44,6 +47,14 @@ public class SnippetDaoTest {
     private UserDao userDao;
     private User user;
 
+    private SimpleJdbcInsert jdbcInsertUser;
+    private SimpleJdbcInsert jdbcInsertSnippet;
+    private SimpleJdbcInsert jdbcInsertLanguage;
+
+    private final String SNIPPETS_TABLE = "snippets";
+    private final String USERS_TABLE = "users";
+    private final String LANGUAGES_TABLE = "languages";
+
     private static final String PASSWORD = "Password";
     private static final String USERNAME = "Username";
     private static final String EMAIL = "email@email.com";
@@ -55,28 +66,65 @@ public class SnippetDaoTest {
     @Before
     public void setUp() {
         jdbcTemplate = new JdbcTemplate(ds);
-        JdbcTestUtils.deleteFromTables(jdbcTemplate, "snippets");
-        JdbcTestUtils.deleteFromTables(jdbcTemplate, "users");
-        long userId = userDao.createUser(USERNAME, PASSWORD, EMAIL, "", 0, DATE.format(Calendar.getInstance().getTime().getTime()));
-        user = userDao.findUserById(userId).get();
-        lang = languageDao.addLanguage("Java");
+
+        jdbcInsertSnippet = new SimpleJdbcInsert(ds).withTableName(SNIPPETS_TABLE).usingGeneratedKeyColumns("id");
+        jdbcInsertUser = new SimpleJdbcInsert(ds).withTableName(USERS_TABLE).usingGeneratedKeyColumns("id");
+        jdbcInsertLanguage = new SimpleJdbcInsert(ds).withTableName(LANGUAGES_TABLE).usingGeneratedKeyColumns("id");
+
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, SNIPPETS_TABLE);
+
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, USERS_TABLE);
+        Map<String, Object> map = new HashMap<String, Object>() {{
+            put("username", USERNAME);
+            put("password", PASSWORD);
+            put("email", EMAIL);
+            put("reputation", 0);
+            put("date_joined", DATE.format(Calendar.getInstance().getTime().getTime()));
+        }};
+        long userId = jdbcInsertUser.executeAndReturnKey(map).longValue();
+        user = new User(userId, USERNAME, PASSWORD, EMAIL, "", 0, DATE.format(Calendar.getInstance().getTime().getTime()), null);
+
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, LANGUAGES_TABLE);
+        Map<String, Object> languageDataMap = new HashMap<String, Object>() {{
+            put("name", "language1");
+        }};
+        long languageId = jdbcInsertLanguage.executeAndReturnKey(languageDataMap).longValue();
+        lang = new Language(languageId, "language1");
     }
 
     @Test
     public void testCreate() {
+        // Ejercitacion
         final long snippetId = snippetDao.createSnippet(user, TITLE, DESCR, CODE, DATE.format(Calendar.getInstance().getTime().getTime()), lang.getId());
+
+        // Evaluacion
         assertEquals(1, JdbcTestUtils.countRowsInTable(jdbcTemplate, "snippets"));
     }
 
     @Test
     public void testFindById() {
-        final long snippetId = snippetDao.createSnippet(user, TITLE, DESCR, CODE, DATE.format(Calendar.getInstance().getTime().getTime()), lang.getId());
+        // Precondiciones
+        JdbcTestUtils.deleteFromTables(jdbcTemplate,SNIPPETS_TABLE);
+        final Map<String, Object> snippetDataMap = new HashMap<String,Object>(){{
+            put("user_id", user.getId());
+            put("title", TITLE);
+            put("description",DESCR);
+            put("code",CODE);
+            put("date_created", DATE.format(Calendar.getInstance().getTime().getTime()));
+            put("language_id", lang.getId());
+        }};
+        final long snippetId = jdbcInsertSnippet.executeAndReturnKey(snippetDataMap).longValue();
+
+        // Ejercitacion
         Optional<Snippet> maybeSnippet = snippetDao.findSnippetById(snippetId);
+
+        //Evaluacion
         assertNotNull(maybeSnippet);
         assertTrue(maybeSnippet.isPresent());
         assertEquals(user.getUsername(), maybeSnippet.get().getOwner().getUsername());
         assertEquals(TITLE, maybeSnippet.get().getTitle());
         assertEquals(DESCR, maybeSnippet.get().getDescription());
     }
+    
 
 }

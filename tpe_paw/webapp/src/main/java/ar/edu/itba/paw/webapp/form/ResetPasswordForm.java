c41d7268a9dbfcee63409c8713fa2881767bee4c
Ignacio Ribas <iribas@itba.edu.ar>
2020-05-04 16:40:11 -0300

    [Change Password] Fix - Added length constraint for new passwords.

--- tpe_paw/webapp/src/main/java/ar/edu/itba/paw/webapp/form/ResetPasswordForm.java
+++ tpe_paw/webapp/src/main/java/ar/edu/itba/paw/webapp/form/ResetPasswordForm.java
@@ -1,11 +1,18 @@
 package ar.edu.itba.paw.webapp.form;
 
 import ar.edu.itba.paw.webapp.validations.FieldMatch;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.Size;
 
 @FieldMatch(first = "newPassword", second = "repeatNewPassword", message = "{FieldMatch.resetPasswordForm.passwords}")
 public class ResetPasswordForm {
 
+    @Size(min=8)
+    @NotBlank
     private String newPassword;
+    @Size(min=8)
+    @NotBlank
     private String repeatNewPassword;
     private String email;
 

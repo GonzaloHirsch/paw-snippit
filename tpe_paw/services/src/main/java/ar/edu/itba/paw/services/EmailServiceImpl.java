bbb55ec73ae7a466eeeef25beb844d8d14ca63c7
Gonzalo Hirsch <ghirsch@itba.edu.ar>
2020-05-04 08:52:45 -0300

    [EmailService] Change in cron syntax to a more standard one

--- tpe_paw/services/src/main/java/ar/edu/itba/paw/services/EmailServiceImpl.java
+++ tpe_paw/services/src/main/java/ar/edu/itba/paw/services/EmailServiceImpl.java
@@ -58,7 +58,7 @@ public class EmailServiceImpl implements EmailService {
         this.sendEmail(to, subject, body);
     }
 
-    @Scheduled(cron = "0 0 12 ? * Mon")
+    @Scheduled(cron = "0 0 12 * * Mon")
     @Override
     public void sendDailyDigest() {
         // Getting timestamp for week before

816f289117ade0cbaa43ad6b0c65eea4b5bbc19c
ahenestrosa <ahenestrosa@itba.edu.ar>
2020-05-16 20:34:13 -0300

    [Migration] Add user and role fields in jpa

--- tpe_paw/models/src/main/java/ar/edu/itba/paw/models/Role.java
+++ tpe_paw/models/src/main/java/ar/edu/itba/paw/models/Role.java
@@ -1,9 +1,23 @@
 package ar.edu.itba.paw.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="roles")
 public class Role {
-    private long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_seq")
+    @SequenceGenerator(allocationSize = 1, sequenceName = "users_id_seq", name="users_id_seq")
+    private Long id;
+
+    @Column(length = 20)
     private String name;
 
+    //For hiberante
+    public Role(){}
+
+    @Deprecated
     public Role(long id, String name) {
         this.id = id;
         this.name = name;
--- tpe_paw/models/src/main/java/ar/edu/itba/paw/models/User.java
+++ tpe_paw/models/src/main/java/ar/edu/itba/paw/models/User.java
@@ -1,19 +1,56 @@
 package ar.edu.itba.paw.models;
 
+import javax.persistence.*;
 import java.util.Locale;
 
+@Entity
+@Table(name = "users")
 public class User {
-    private long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_seq")
+    @SequenceGenerator(allocationSize = 1, sequenceName = "users_id_seq", name="users_id_seq")
+    private Long id;
+
+    @Column(length = 30)
     private String username;
+
+    @Column(length=60)
     private String password;
+
+    @Column(length = 60)
     private String email;
+
+    @Column(length = 300)
     private String description;
+
+    @Column
     private int reputation;
+
+    @Column
     private String dateJoined;
+
+    @Column
     private byte[] icon;
-    private Locale locale;
+
+    @Column
     private boolean verified;
 
+    @Column(length = 5)
+    private String lang = "EN"; //Set EN as default value
+
+    @Column(length = 5)
+    private String region = "US";
+
+
+    @Deprecated
+    private Locale locale;
+
+
+    // For Hibernate
+    public User() {}
+
+    @Deprecated
     public User(long id, String username, String password, String email, String dateJoined, byte[] icon, Locale locale, boolean verified) {
         this.id = id;
         this.username = username;
@@ -27,6 +64,7 @@ public class User {
         this.verified = verified;
     }
 
+    @Deprecated
     public User(long id, String username, String password, String email, String description, int reputation, String dateJoined, byte[] icon, Locale locale, boolean verified) {
         this.id = id;
         this.username = username;
@@ -68,12 +106,20 @@ public class User {
 
     public byte[] getIcon() { return this.icon; }
 
-    public Locale getLocale() {
-        return locale;
+    public String getLang() {
+        return lang;
     }
 
-    public void setLocale(Locale locale) {
-        this.locale = locale;
+    public String getRegion() {
+        return region;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
     }
 
     public boolean isVerified() {

dc835f00df3339f3a677ce305cebbd24670f5e76
Gonzalo Hirsch <ghirsch@itba.edu.ar>
2020-05-27 16:06:31 -0300

    [Fix] Made final and static helper classes

--- tpe_paw/services/src/main/java/ar/edu/itba/paw/services/helpers/crypto/HashGenerator.java
+++ tpe_paw/services/src/main/java/ar/edu/itba/paw/services/helpers/crypto/HashGenerator.java
@@ -5,25 +5,17 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
-public class HashGenerator {
-
-    private static HashGenerator instance;
-
-    public static HashGenerator getInstance() {
-        if (instance == null)
-            instance = new HashGenerator();
-        return instance;
-    }
+public final class HashGenerator {
 
     private HashGenerator() {
     }
 
-    public String generateRecoveryToken(String code){
-        return this.process(code.getBytes(StandardCharsets.UTF_8));
+    public static String generateRecoveryToken(String code){
+        return process(code.getBytes(StandardCharsets.UTF_8));
     }
 
-    public String generateSecretKey(String email, String pass){
-        return this.process((email + pass).getBytes(StandardCharsets.UTF_8));
+    public static String generateSecretKey(String email, String pass){
+        return process((email + pass).getBytes(StandardCharsets.UTF_8));
     }
 
     /**
@@ -31,7 +23,7 @@ public class HashGenerator {
      * @param data Data bytes to be processed
      * @return Base64 url-safe encoding
      */
-    private String process(byte[] data){
+    private static String process(byte[] data){
         try {
             MessageDigest sha256Digest = MessageDigest.getInstance("SHA-256");
             byte[] userPassHash = sha256Digest.digest(data);

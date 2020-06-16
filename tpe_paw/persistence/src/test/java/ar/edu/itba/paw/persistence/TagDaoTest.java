37e2e90d0047637be06b824605f4904d650d96ea
Florencia Petrikovich <fpetrikovich@itba.edu.ar>
2020-06-16 20:58:39 -0300

    [Fix][TagDaoTest] Fixed broken test due to the addition of showFollowing and added new ones to test this new feature

--- tpe_paw/persistence/src/test/java/ar/edu/itba/paw/persistence/TagDaoTest.java
+++ tpe_paw/persistence/src/test/java/ar/edu/itba/paw/persistence/TagDaoTest.java
@@ -1,235 +1,289 @@
-//package ar.edu.itba.paw.persistence;
-//
-//import ar.edu.itba.paw.interfaces.dao.TagDao;
-//import ar.edu.itba.paw.models.Language;
-//import ar.edu.itba.paw.models.Snippet;
-//import ar.edu.itba.paw.models.Tag;
-//import ar.edu.itba.paw.models.User;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.jdbc.JdbcTestUtils;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.PersistenceContext;
-//import javax.sql.DataSource;
-//import java.time.Instant;
-//import java.util.*;
-//import java.util.stream.Collectors;
-//
-//
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = TestConfig.class)
-//@Transactional
-//public class TagDaoTest {
-//
-//    @Autowired private DataSource ds;
-//    @Autowired private TagDao tagDao;
-//
-//    @PersistenceContext
-//    private EntityManager em;
-//
-//
-//    private User user;
-//    private Language language;
-//    private Snippet snippet;
-//
-//    @Before
-//    public void setUp() {
-//        user = TestMethods.insertUser(em, TestConstants.USER_USERNAME, TestConstants.USER_PASSWORD, TestConstants.USER_EMAIL, TestConstants.USER_DATE, TestConstants.LOCALE_EN, TestConstants.USER_VERIFIED);
-//        language = TestMethods.insertLanguage(em, TestConstants.LANGUAGE);
-//        snippet = TestMethods.insertSnippet(em, user, TestConstants.SNIPPET_TITLE, TestConstants.SNIPPET_DESCR, TestConstants.SNIPPET_CODE, Instant.now(), language, Collections.emptyList(), false, false);
-//    }
-//
-//
-//    @Test
-//    @Transactional
-//    public void testAddTag(){
-//        Tag maybeTag = tagDao.addTag(TestConstants.TAG);
-//
-//        Assert.assertNotNull(maybeTag);
-//        Assert.assertEquals(TestConstants.TAG, maybeTag.getName());
-//    }
-//
-//   @Test
-//   @Transactional
-//    public void testAddTagS(){
-//        List<String> stringList = Arrays.asList(TestConstants.TAG,TestConstants.TAG2,null);
-//
-//        tagDao.addTags(stringList);
-//    }
-//
-//    @Test
-//    public void testAddTagSEmpty(){
-//        List<String> stringList = new ArrayList<>();
-//
-//        tagDao.addTags(stringList);
-//    }
-//
-//    @Test
-//    public void testFindById(){
-//        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
-//
-//        Optional<Tag> maybeTag = tagDao.findById(tag.getId());
-//
-//        Assert.assertTrue(maybeTag.isPresent());
-//        Assert.assertEquals(tag.getId(), maybeTag.get().getId());
-//        Assert.assertEquals(TestConstants.TAG,maybeTag.get().getName());
-//    }
-//
-//    @Test
-//    public void testFindByIdEmpty(){
-//        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
-//
-//        Optional<Tag> maybeTag = tagDao.findById(-1);
-//
-//        Assert.assertFalse(maybeTag.isPresent());
-//    }
-//
-//    @Test
-//    public void testFindByName(){
-//        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
-//
-//        Optional<Tag> maybeTag = tagDao.findByName(TestConstants.TAG);
-//
-//        Assert.assertTrue(maybeTag.isPresent());
-//        Assert.assertEquals(tag.getId(), maybeTag.get().getId());
-//        Assert.assertEquals(TestConstants.TAG,maybeTag.get().getName());
-//    }
-//
-//    @Test
-//    public void testFindByNameEmpty(){
-//        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
-//
-//        Optional<Tag> maybeTag = tagDao.findByName(TestConstants.TAG2);
-//
-//        Assert.assertFalse(maybeTag.isPresent());
-//    }
-//
-//    @Test
-//    public void testGetAllTags(){
-//        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
-//        Tag tag2 = TestMethods.insertTag(em, TestConstants.TAG2);
-//
-//        Collection<Tag> maybeTags = tagDao.getAllTags();
-//
-//        Assert.assertNotNull(maybeTags);
-//        Assert.assertEquals(2,maybeTags.size());
-//        List<Long> maybeList = maybeTags.stream().mapToLong(Tag::getId).boxed().collect(Collectors.toList());
-//        Assert.assertTrue(maybeList.contains(tag.getId()));
-//        Assert.assertTrue(maybeList.contains(tag2.getId()));
-//    }
-//
-//    @Test
-//    public void testGetAllTagsEmpty() {
-//        Collection<Tag> maybeTags = tagDao.getAllTags();
-//
-//        Assert.assertNotNull(maybeTags);
-//        Assert.assertEquals(0, maybeTags.size());
-//    }
-//
-//    //TODO: Fix why fails
-//
-//    @Test
-//    @Transactional
-//    public void testRemoveTag(){
-//        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
-//
-//        tagDao.removeTag(tag.getId());
-//    }
-//
-//    @Test
-//    public void testRemoveTagEmpty(){
-//        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
-//
-//        tagDao.removeTag(tag.getId());
-//    }
-//
-//
-//    @Test
-//    public void testGetAllTagsCountByName(){
-//        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
-//        Tag tag2 = TestMethods.insertTag(em, TestConstants.TAG2);
-//
-//        int res1 = tagDao.getAllTagsCountByName(TestConstants.TAG, true);
-//        int res2 = tagDao.getAllTagsCountByName(TestConstants.TAG2, true);
-//
-//        Assert.assertEquals(1,res1);
-//        Assert.assertEquals(1,res2);
-//    }
-//
-//    @Test
-//    public void testGetAllTagsCountByNameEmpty(){
-//        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
-//        Tag tag2 = TestMethods.insertTag(em, TestConstants.TAG2);
-//
-//        int res = tagDao.getAllTagsCountByName("Not a tag name-xxxxx", true);
-//
-//        Assert.assertEquals(0,res);
-//    }
-//
-//    @Test
-//    public void testGetAllTagsCount(){
-//        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
-//        Tag tag2 = TestMethods.insertTag(em, TestConstants.TAG2);
-//
-//        int res = tagDao.getAllTagsCount(true);
-//
-//        Assert.assertEquals(2,res);
-//    }
-//
-//    @Test
-//    public void testGetAllTagsCountEmpty(){
-//        int res = tagDao.getAllTagsCount(true);
-//
-//        Assert.assertEquals(0,res);
-//    }
-//
-//    @Test
-//    public void testFindTagsByName(){
-//        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
-//        Tag tag2 = TestMethods.insertTag(em, TestConstants.TAG2);
-//
-//        Collection<Tag> maybeCollection = tagDao.findTagsByName("tag", true,1,TestConstants.TAG_PAGE_SIZE);
-//
-//        Assert.assertNotNull(maybeCollection);
-//        Assert.assertEquals(2,maybeCollection.size());
-//        List<Long> idCol = maybeCollection.stream().mapToLong(Tag::getId).boxed().collect(Collectors.toList());
-//        Assert.assertTrue(idCol.contains(tag.getId()));
-//        Assert.assertTrue(idCol.contains(tag2.getId()));
-//    }
-//
-//    @Test
-//    public void testFindTagsByNameNotShowEmpty(){
-//        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
-//        Tag tag2 = TestMethods.insertTag(em, TestConstants.TAG2);
-//
-//        Collection<Tag> maybeCollection = tagDao.findTagsByName("tag", false,1,TestConstants.TAG_PAGE_SIZE);
-//
-//        Assert.assertNotNull(maybeCollection);
-//        Assert.assertEquals(0,maybeCollection.size());
-//    }
-//
-//    @Test
-//    public void testFindTagsByNameEmpty(){
-//        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
-//        Tag tag2 = TestMethods.insertTag(em, TestConstants.TAG2);
-//
-//        Collection<Tag> maybeCollection = tagDao.findTagsByName("notatagname-xxxx", true,1,TestConstants.TAG_PAGE_SIZE);
-//
-//        Assert.assertNotNull(maybeCollection);
-//        Assert.assertEquals(0,maybeCollection.size());
-//
-//    }
-//
-//
-//
-//}
+package ar.edu.itba.paw.persistence;
+
+import ar.edu.itba.paw.interfaces.dao.TagDao;
+import ar.edu.itba.paw.models.Language;
+import ar.edu.itba.paw.models.Snippet;
+import ar.edu.itba.paw.models.Tag;
+import ar.edu.itba.paw.models.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.time.Instant;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+@Transactional
+public class TagDaoTest {
+
+    @Autowired private TagDao tagDao;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Before
+    public void setUp() {
+    }
+
+
+    @Test
+    @Transactional
+    public void testAddTag(){
+        Tag maybeTag = tagDao.addTag(TestConstants.TAG);
+
+        Assert.assertNotNull(maybeTag);
+        Assert.assertEquals(TestConstants.TAG, maybeTag.getName());
+    }
+
+   @Test
+   @Transactional
+    public void testAddTagS(){
+        List<String> stringList = Arrays.asList(TestConstants.TAG,TestConstants.TAG2,null);
+
+        tagDao.addTags(stringList);
+    }
+
+    @Test
+    public void testAddTagSEmpty(){
+        List<String> stringList = new ArrayList<>();
+
+        tagDao.addTags(stringList);
+    }
+
+    @Test
+    public void testFindById(){
+        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
+
+        Optional<Tag> maybeTag = tagDao.findById(tag.getId());
+
+        Assert.assertTrue(maybeTag.isPresent());
+        Assert.assertEquals(tag.getId(), maybeTag.get().getId());
+        Assert.assertEquals(TestConstants.TAG,maybeTag.get().getName());
+    }
+
+    @Test
+    public void testFindByIdEmpty(){
+        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
+
+        Optional<Tag> maybeTag = tagDao.findById(-1);
+
+        Assert.assertFalse(maybeTag.isPresent());
+    }
+
+    @Test
+    public void testFindByName(){
+        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
+
+        Optional<Tag> maybeTag = tagDao.findByName(TestConstants.TAG);
+
+        Assert.assertTrue(maybeTag.isPresent());
+        Assert.assertEquals(tag.getId(), maybeTag.get().getId());
+        Assert.assertEquals(TestConstants.TAG,maybeTag.get().getName());
+    }
+
+    @Test
+    public void testFindByNameEmpty(){
+        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
+
+        Optional<Tag> maybeTag = tagDao.findByName(TestConstants.TAG2);
+
+        Assert.assertFalse(maybeTag.isPresent());
+    }
+
+    @Test
+    public void testGetAllTags(){
+        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
+        Tag tag2 = TestMethods.insertTag(em, TestConstants.TAG2);
+
+        Collection<Tag> maybeTags = tagDao.getAllTags();
+
+        Assert.assertNotNull(maybeTags);
+        Assert.assertEquals(2,maybeTags.size());
+        List<Long> maybeList = maybeTags.stream().mapToLong(Tag::getId).boxed().collect(Collectors.toList());
+        Assert.assertTrue(maybeList.contains(tag.getId()));
+        Assert.assertTrue(maybeList.contains(tag2.getId()));
+    }
+
+    @Test
+    public void testGetAllTagsEmpty() {
+        Collection<Tag> maybeTags = tagDao.getAllTags();
+
+        Assert.assertNotNull(maybeTags);
+        Assert.assertEquals(0, maybeTags.size());
+    }
+
+    //TODO: Fix why fails
+
+    @Test
+    @Transactional
+    public void testRemoveTag(){
+        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
+
+        tagDao.removeTag(tag.getId());
+    }
+
+    @Test
+    public void testRemoveTagEmpty(){
+        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
+
+        tagDao.removeTag(tag.getId());
+    }
+
+
+    @Test
+    public void testGetAllTagsCountByName(){
+        TestMethods.insertTag(em, TestConstants.TAG);
+        TestMethods.insertTag(em, TestConstants.TAG2);
+
+        int res1 = tagDao.getAllTagsCountByName(TestConstants.TAG, true, false, null);
+        int res2 = tagDao.getAllTagsCountByName(TestConstants.TAG2, true, false, null);
+
+        Assert.assertEquals(1,res1);
+        Assert.assertEquals(1,res2);
+    }
+
+    @Test
+    public void testGetAllTagsCountByNameEmpty(){
+        TestMethods.insertTag(em, TestConstants.TAG);
+        TestMethods.insertTag(em, TestConstants.TAG2);
+
+        int res = tagDao.getAllTagsCountByName("Not a tag name-xxxxx", true, false, null);
+
+        Assert.assertEquals(0,res);
+    }
+
+    @Test
+    public void testGetAllTagsCount(){
+        TestMethods.insertTag(em, TestConstants.TAG);
+        TestMethods.insertTag(em, TestConstants.TAG2);
+
+        int res = tagDao.getAllTagsCount(true, false, null);
+
+        Assert.assertEquals(2,res);
+    }
+
+    @Test
+    public void testGetAllTagsCountEmpty(){
+        int res = tagDao.getAllTagsCount(true, false, null);
+
+        Assert.assertEquals(0,res);
+    }
+
+    @Test
+    public void testFindTagsByName(){
+        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
+        Tag tag2 = TestMethods.insertTag(em, TestConstants.TAG2);
+
+        Collection<Tag> collection = tagDao.findTagsByName("tag", true, false, null, 1,TestConstants.TAG_PAGE_SIZE);
+
+        Assert.assertNotNull(collection);
+        Assert.assertEquals(2,collection.size());
+        List<Long> idCol = collection.stream().mapToLong(Tag::getId).boxed().collect(Collectors.toList());
+        Assert.assertTrue(idCol.contains(tag.getId()));
+        Assert.assertTrue(idCol.contains(tag2.getId()));
+    }
+
+    @Test
+    public void testFindTagsByNameNotShowEmpty(){
+        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
+        Tag tag2 = TestMethods.insertTag(em, TestConstants.TAG2);
+
+        Collection<Tag> collection = tagDao.findTagsByName("tag", false, false, null,1,TestConstants.TAG_PAGE_SIZE);
+
+        Assert.assertNotNull(collection);
+        Assert.assertEquals(0,collection.size());
+    }
+
+    @Test
+    public void testFindTagsByNameEmpty(){
+        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
+        Tag tag2 = TestMethods.insertTag(em, TestConstants.TAG2);
+
+        Collection<Tag> collection = tagDao.findTagsByName("notatagname-xxxx", true, false, null, 1,TestConstants.TAG_PAGE_SIZE);
+
+        Assert.assertNotNull(collection);
+        Assert.assertEquals(0,collection.size());
+
+    }
+
+    @Test
+    public void testFindTagsByNameWithUserShowAll(){
+        User user = TestMethods.insertUser(em, TestConstants.USER_USERNAME, TestConstants.USER_PASSWORD, TestConstants.USER_EMAIL, Instant.now(),TestConstants.LOCALE_EN,false);
+
+        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
+        Tag tag2 = TestMethods.insertTag(em, TestConstants.TAG2);
+        Tag tag3 = TestMethods.insertTag(em, TestConstants.TAG3);
+
+        TestMethods.setUserFollowingTags(em, user, Arrays.asList(tag, tag2));
+
+        Collection<Tag> collection = tagDao.findTagsByName(TestConstants.TAG_TERM, true, false, user.getId(), 1,TestConstants.TAG_PAGE_SIZE);
+
+        Assert.assertNotNull(collection);
+        Assert.assertEquals(3,collection.size());
+    }
+
+    @Test
+    public void testFindTagsByNameShowFollowing(){
+        User user = TestMethods.insertUser(em, TestConstants.USER_USERNAME, TestConstants.USER_PASSWORD, TestConstants.USER_EMAIL, Instant.now(),TestConstants.LOCALE_EN,false);
+
+        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
+        Tag tag2 = TestMethods.insertTag(em, TestConstants.TAG2);
+        Tag tag3 = TestMethods.insertTag(em, TestConstants.TAG3);
+
+        TestMethods.setUserFollowingTags(em, user, Arrays.asList(tag, tag2));
+
+        Collection<Tag> collection = tagDao.findTagsByName(TestConstants.TAG_TERM, true, true, user.getId(), 1,TestConstants.TAG_PAGE_SIZE);
+
+        Assert.assertNotNull(collection);
+        Assert.assertEquals(2,collection.size());
+    }
+
+    @Test
+    public void testFindTagsByNameShowFollowingHideEmpty(){
+        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
+        Tag tag2 = TestMethods.insertTag(em, TestConstants.TAG2);
+        Tag tag3 = TestMethods.insertTag(em, TestConstants.TAG3);
+
+        User user = TestMethods.insertUser(em, TestConstants.USER_USERNAME, TestConstants.USER_PASSWORD, TestConstants.USER_EMAIL, Instant.now(),TestConstants.LOCALE_EN,false);
+        Language language = TestMethods.insertLanguage(em, TestConstants.LANGUAGE);
+        Snippet snippet = TestMethods.insertSnippet(em, user, TestConstants.SNIPPET_TITLE, TestConstants.SNIPPET_DESCR, TestConstants.SNIPPET_CODE, Instant.now(), language, Collections.singletonList(tag), TestConstants.SNIPPET_FLAGGED, false);
+
+        TestMethods.setUserFollowingTags(em, user, Arrays.asList(tag, tag2));
+
+        Collection<Tag> collection = tagDao.findTagsByName(TestConstants.TAG_TERM, false, true, user.getId(), 1,TestConstants.TAG_PAGE_SIZE);
+
+        Assert.assertNotNull(collection);
+        Assert.assertEquals(1,collection.size());
+    }
+
+    @Test
+    public void testFindTagsByNameShowFollowingHideEmptyNoSnippets(){
+        Tag tag = TestMethods.insertTag(em, TestConstants.TAG);
+        Tag tag2 = TestMethods.insertTag(em, TestConstants.TAG2);
+        Tag tag3 = TestMethods.insertTag(em, TestConstants.TAG3);
+
+        User user = TestMethods.insertUser(em, TestConstants.USER_USERNAME, TestConstants.USER_PASSWORD, TestConstants.USER_EMAIL, Instant.now(),TestConstants.LOCALE_EN,false);
+        Language language = TestMethods.insertLanguage(em, TestConstants.LANGUAGE);
+        Snippet snippet = TestMethods.insertSnippet(em, user, TestConstants.SNIPPET_TITLE, TestConstants.SNIPPET_DESCR, TestConstants.SNIPPET_CODE, Instant.now(), language, Collections.singletonList(tag3), TestConstants.SNIPPET_FLAGGED, false);
+
+        TestMethods.setUserFollowingTags(em, user, Arrays.asList(tag, tag2));
+
+        Collection<Tag> collection = tagDao.findTagsByName(TestConstants.TAG_TERM, false, true, user.getId(), 1,TestConstants.TAG_PAGE_SIZE);
+
+        Assert.assertNotNull(collection);
+        Assert.assertTrue(collection.isEmpty());
+    }
+
+
+}

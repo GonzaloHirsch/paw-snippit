83074020ae57ee85e3bd3cdcb62e140d99ed1d45
Florencia Petrikovich <fpetrikovich@itba.edu.ar>
2020-07-27 16:44:05 -0300

    [DTO] LanguageDto added

--- tpe_paw/webapp/src/main/java/ar/edu/itba/paw/webapp/dto/LanguageDto.java
+++ tpe_paw/webapp/src/main/java/ar/edu/itba/paw/webapp/dto/LanguageDto.java
@@ -2,11 +2,34 @@ package ar.edu.itba.paw.webapp.dto;
 
 import ar.edu.itba.paw.models.Language;
 
-public class LanguageDto {
+import javax.ws.rs.core.UriInfo;
 
-    public static LanguageDto fromLanguage(Language language){
+public class LanguageDto {
+    private String name;
+    private Long id;
+
+    public static LanguageDto fromLanguage(Language language, UriInfo uriInfo){
         final LanguageDto dto = new LanguageDto();
 
+        dto.id = language.getId();
+        dto.name = language.getName();
+
         return dto;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }

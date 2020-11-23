a709d58c4fbfb153acf80d919005f5b0b9743354
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2020-11-23 11:07:59 -0300

    [React][Styles] Added SASS to project

--- tpe_paw/react_ui/src/App.js
+++ tpe_paw/react_ui/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import NavBar from './components/navigation/navbar';
 import SnippetCard from './components/snippets/snippet_card'
 

2b9cca954db02727fe63ba526b719ad0fc695ab6
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2020-11-23 11:26:54 -0300

    [React][Setup] Added missing JQuery import

--- tpe_paw/react_ui/src/components/navigation/navbar.jsx
+++ tpe_paw/react_ui/src/components/navigation/navbar.jsx
@@ -32,7 +32,7 @@ class NavBar extends Component {
                 </div>
                 </li>
                 <li className="nav-item">
-                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
                 </li>
             </ul>
             <form className="form-inline my-2 my-lg-0">

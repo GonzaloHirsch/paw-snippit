3a5ca6a6beea6a9fc8d4f3278c743eb74112444a
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2020-11-23 12:52:18 -0300

    [React][Navigation] Added sidebar like old version
    Needs style still

--- tpe_paw/react_ui/src/components/navigation/navbar.jsx
+++ tpe_paw/react_ui/src/components/navigation/navbar.jsx
@@ -1,48 +1,135 @@
-import React, { Component } from 'react';
-import i18n from '../../i18n';
+import React, { Component } from "react";
+import i18n from "../../i18n";
+import Icon from "@mdi/react";
+import { mdiClose, mdiMenu } from "@mdi/js";
 
 // Component -> https://getbootstrap.com/docs/4.5/components/navbar/
 class NavBar extends Component {
-    state = {  }
-    render() { 
-        return ( 
+  state = {
+    navIsOpen: false,
+  };
+
+  // Change status of the nav variable
+  navInteract = (isNavOpen) => {
+    if (isNavOpen === true) {
+      document.getElementById("mySidenav").style.width = "0";
+    } else {
+      document.getElementById("mySidenav").style.width = "180px";
+    }
+    this.setState({ navIsOpen: !isNavOpen });
+  };
+
+  // Method to get the right navigation icon
+  getNavIcon = (isNavOpen) => {
+    if (isNavOpen === true) {
+      return <Icon path={mdiClose} size={1} />;
+    } else {
+      return <Icon path={mdiMenu} size={1} />;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <div id="mySidenav" className="sidenav">
+          <a href="#">About</a>
+          <a href="#">Services</a>
+          <a href="#">Clients</a>
+          <a href="#">Contact</a>
+        </div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Snippit</a>
-            {i18n.t('test')}
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="btn btn-sm"
+            type="button"
+            data-toggle="collapse"
+            aria-expanded="false"
+            aria-label="Toggle side navigation"
+            onClick={() => this.navInteract(this.state.navIsOpen)}
+          >
+            {this.getNavIcon(this.state.navIsOpen)}
+          </button>
+          <a className="navbar-brand" href="#">
+            Snippit
+          </a>
+          {i18n.t("test")}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Home <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Dropdown
                 </a>
                 <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                  <div className="dropdown-divider"></div>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
                 </div>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-                </li>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link disabled"
+                  href="#"
+                  tabIndex="-1"
+                  aria-disabled="true"
+                >
+                  Disabled
+                </a>
+              </li>
             </ul>
             <form className="form-inline my-2 my-lg-0">
-                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button
+                className="btn btn-outline-success my-2 my-sm-0"
+                type="submit"
+              >
+                Search
+              </button>
             </form>
-            </div>
-        </nav> 
-      );
-    }
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
- 
+
 export default NavBar;

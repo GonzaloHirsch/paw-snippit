01001fb9b0fb9a98f14a8364ddc96ccb7db56a6a
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2020-12-10 16:32:39 -0300

    [Correction] Missing searchFromUrl frunction in Upvoted

--- tpe_paw/react_ui/src/components/pages/upvoted.jsx
+++ tpe_paw/react_ui/src/components/pages/upvoted.jsx
@@ -1,6 +1,7 @@
 import SnippetFeedHOC from "./snippet_feed_hoc";
 import SnippetUpvotedFeed from "../snippets/snippet_upvoted_feed";
 import store from "../../store";
+import { getNavSearchFromUrl } from "../../js/search_from_url";
 
 const userId = store.getState().auth.info.uid;
 
@@ -8,7 +9,8 @@ const Upvoted = SnippetFeedHOC(
   SnippetUpvotedFeed,
   (SnippetFeedClient, page) => SnippetFeedClient.getUpvotedSnippetFeed(page, userId),
   (SnippetFeedClient, page, search) =>
-    SnippetFeedClient.searchUpvotedSnippetFeed(page, userId, search)
+    SnippetFeedClient.searchUpvotedSnippetFeed(page, userId, search),
+    (url) => getNavSearchFromUrl(url)
 );
 
 export default Upvoted;

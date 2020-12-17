ab0db87b5f88a5295375b172ab412f21cdf3bdfb
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2020-12-17 08:21:11 -0300

    [DefaultProfileImage] Correction to obtaining the default profile image

--- tpe_paw/react_ui/src/js/snippet_utils.js
+++ tpe_paw/react_ui/src/js/snippet_utils.js
@@ -1,8 +1,9 @@
 export function getUserProfilePicUrl(creator) {
+  console.log(process.env.PUBLIC_URL, "URL")
   if (creator.hasPicture) {
     return creator.picture;
   }
-  return "/userIcon.jpg";
+  return process.env.PUBLIC_URL + "/userIcon.jpg";
 }
 
 export function getSnippetPositionInArray(snippets, id) {

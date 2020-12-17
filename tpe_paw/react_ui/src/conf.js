ef1c570c0a95fa75ec5f35c2617e18e8205fa7cc
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2020-12-17 08:14:29 -0300

    [Setup] Setup for production deploy

--- tpe_paw/react_ui/src/conf.js
+++ tpe_paw/react_ui/src/conf.js
@@ -1,7 +1,7 @@
 const conf = {
-    API_DOMAIN:'pawserver.it.itba.edu.ar/paw-2020a-2/',
+    API_DOMAIN:'pawserver.it.itba.edu.ar/paw-2020a-2',
     API_PROTOCOL:'http://',
-    API_PORT:':8080', 
+    API_PORT:'', 
     API_BASE:'/api/v1/'
   };
   
